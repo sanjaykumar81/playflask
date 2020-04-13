fdfed226b60601433e5c0edc2d1edb71cc57b835
sanjaykmruk <sanjay.kumar@armakuni.com>
2020-04-13 13:16:01 +0100

    added example for url_for method'

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask('__name__')
 
@@ -7,12 +7,56 @@ app = Flask('__name__')
 def hello():
     return "Welcome to flask"
 
+
 """
-Example for working with the path param
+Example - working with the path param
 """
+
+
 @app.route("/hello/<name>")
 def hello_path_param(name):
     return f"welcome '{name}' to flask world"
+
+
+"""
+Example - working with the path param with type
+"""
+
+
+@app.route("/hello/<int:number>")
+def hello_path_param_type(number):
+    return f"You have add {number} to flask world"
+
+
+"""
+Example - working with the '/' in the uri. '/' in end let you use the uri with and without '/'.
+'/slash/' == '/slash' is True
+"""
+
+
+@app.route("/slash/")
+def hello_slash():
+    return f"welcome slash in path of flask world"
+
+
+"""
+Example - url_for() is use to dynamically redirect request to other functions  
+"""
+@app.route("/admin/<name>")
+def hello_admin(name):
+    return f"Welcome {name}. You are admin of flask world"
+
+@app.route("/guest/<name>")
+def hello_guest(name):
+    if name == "sanjay":
+        return redirect(url_for("hello_admin",name = name))
+    else:
+        return redirect(url_for("hello_user", user=name))
+
+@app.route("/user/<user>")
+def hello_user(user):
+    return f"Welcome {user}. You are user of flask world"
+
 
 if __name__ == "__main__":
     app.run()
