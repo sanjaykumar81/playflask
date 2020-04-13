73426709d8d9c6afcfdf68783401d56c6e560513
sanjaykmruk <sanjay.kumar@armakuni.com>
2020-04-13 12:47:51 +0100

    path param

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask('__name__')
 
+
 @app.route("/")
 def hello():
     return "Welcome to flask"
+
+"""
+Example for working with the path param
+"""
+@app.route("/hello/<name>")
+def hello_path_param(name):
+    return f"welcome '{name}' to flask world"
 
 if __name__ == "__main__":
     app.run()
